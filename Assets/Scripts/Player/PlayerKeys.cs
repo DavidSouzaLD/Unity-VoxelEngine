0a2d56a32a1f987e6af1cd175a0fc2168f89d58f
David Allison <Davidsouza.jobs@gmail.com>
2022-10-30 18:01:36 -0300

    Update PlayerKeys.cs

--- Assets/Scripts/Player/PlayerKeys.cs
+++ Assets/Scripts/Player/PlayerKeys.cs
@@ -5,6 +5,7 @@ namespace Game.Player
     public class PlayerKeys : Singleton<PlayerKeys>
     {
         [Header("Keys")]
+        [SerializeField] private KeyCode downKey = KeyCode.LeftControl;
         [SerializeField] private KeyCode runKey = KeyCode.LeftShift;
         [SerializeField] private KeyCode jumpKey = KeyCode.Space;
         [SerializeField] private KeyCode crouchKey = KeyCode.LeftControl;
@@ -13,6 +14,7 @@ namespace Game.Player
         [SerializeField] private KeyCode openDebug = KeyCode.BackQuote;
 
         // Static
+        public static KeyCode Down { get { return Instance.downKey; } }
         public static KeyCode Run { get { return Instance.runKey; } }
         public static KeyCode Jump { get { return Instance.jumpKey; } }
         public static KeyCode Crouch { get { return Instance.crouchKey; } }
