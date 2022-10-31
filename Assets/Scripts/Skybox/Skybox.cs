7b7fdadb5ae15e2ea262077df7a933a227dd9cc5
David Allison <Davidsouza.jobs@gmail.com>
2022-10-31 10:49:00 -0300

    TimeOfDay fixed

--- Assets/Scripts/Skybox/Skybox.cs
+++ Assets/Scripts/Skybox/Skybox.cs
@@ -3,40 +3,50 @@ using QFSW.QC;
 
 public class Skybox : MonoBehaviour
 {
-    [Header("Settings")]
-    public bool useRotationSun;
-    public Light sunLight;
-    public Transform sun;
-    [Command("Time.SetSpeed")]
-    public float timeSpeed;
+    [Header("Time")]
+    [Range(0, 24)]
+    [Command("Time.SetTime")]
+    [CommandDescription("Sets the current time value. 0 - 24")]
+    public float timeOfDay;
+    public float lenghtOfDay = 1f;
+
+    [Header("Sun & Moon")]
+    public Light directionalLight;
+    public Transform sunTransform;
+    public Transform moonTransform;
 
     [Header("Colors")]
+    public Gradient colorLight;
     public Gradient colorSky;
     public Gradient colorHorizon;
     public Material skyboxMaterial;
 
-    string shaderName = "_SunDirection";
-
     private void Start()
     {
-        sun.position = -sunLight.transform.forward * 1500f;
+        sunTransform.position = -directionalLight.transform.forward * 1800f;
+        moonTransform.position = directionalLight.transform.forward * 1800f;
     }
 
     private void LateUpdate()
     {
-        if (useRotationSun)
-        {
-            // Rotate lighing sun
-            sunLight.transform.Rotate(new Vector3(1f, 0f, 0f) * Time.deltaTime * timeSpeed);
-        }
+        timeOfDay += 6.0f / lenghtOfDay * 4f * Time.deltaTime;
+        timeOfDay %= 24;
+        UpdateSkybox(timeOfDay / 24f);
+    }
+
+    private void UpdateSkybox(float timePercent)
+    {
+        // Rotate lighing
+        directionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170, 0));
 
         // Apply values of light
-        skyboxMaterial.SetColor("_SkyColor", colorSky.Evaluate(sunLight.transform.localEulerAngles.x / 180f));
-        skyboxMaterial.SetColor("_HorizonColor", colorHorizon.Evaluate(sunLight.transform.localEulerAngles.x / 180f));
-        skyboxMaterial.SetFloat("_WindSpeed", timeSpeed / 2f);
+        directionalLight.color = colorLight.Evaluate(timePercent);
+        skyboxMaterial.SetColor("_SkyColor", colorSky.Evaluate(timePercent));
+        skyboxMaterial.SetColor("_HorizonColor", colorHorizon.Evaluate(timePercent));
+        skyboxMaterial.SetFloat("_WindSpeed", lenghtOfDay / 2f);
 
         // Active stars if is night
-        if (sunLight.transform.localEulerAngles.x > 180f)
+        if (directionalLight.transform.localEulerAngles.x > 180f)
         {
             skyboxMaterial.SetFloat("_StarsOpacity", Mathf.Lerp(skyboxMaterial.GetFloat("_StarsOpacity"), 1f, Time.deltaTime * 2f));
         }
